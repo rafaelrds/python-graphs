a3cc21f46e3ef4df2f0c2a1ff26b07f13e794690
Rafael Rêgo <rafael.rego@loggi.com>
2019-07-28 23:40:15 -0300

    Created function for generating edges and finding isolated vertexes

--- first_example.py
+++ first_example.py
@@ -6,3 +6,27 @@ graph = {
     "e": ["c", "b"],
     "f": []
 }
+
+
+def generate_edges(graph):
+    edges = []
+    for node in graph:
+        for neighbour in graph[node]:
+            edges.append((node, neighbour))
+
+    return edges
+
+
+print(generate_edges(graph))
+
+
+def find_isolated_nodes(graph):
+    """ returns a list of isolated nodes. """
+    isolated = []
+    for node in graph:
+        if not graph[node]:
+            isolated += node
+    return isolated
+
+
+print(find_isolated_nodes(graph))
